94640234ddef27c6ef1de6494063237d89ab9ebe
Manuel Rojas <manuelrojasb57@gmail.com>
2022-11-02 23:57:05 -0500

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Manuel Antonio Rojas Barrero on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
